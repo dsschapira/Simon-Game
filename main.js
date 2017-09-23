0a331c358df1f4f3723b0e48e4dfb42081dcb0b0
dsschapira <dsschapira@gmail.com>
2017-09-23 14:57:07 -0500

    Made on button initialize Game object.  Formatted JS code.

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
+//Define Classes
+class Game {
+    constructor(){
+        this.init(); //make the constructor call init so that we can wipe the object if necessary.
+    }
+    init(){
+        this.strict = false;
+        this.sequence = [];
+    }
+    addToSequence(val){
+        this.sequence.push(val);
+    }
+}
+//End Classes
 
-document.addEventListener("DOMContentLoaded", function() {
+//Globals
+TURNED_ON = false;
+//End Globals
 
-    document.querySelector('#on-slider').addEventListener('click',function(e){
-        document.querySelector("#on-slider").classList.toggle('game-turned-on');
+
+//Begin Callable Functions
+
+function turnOnOff(game){
+    document.querySelector("#on-slider").classList.toggle('game-turned-on');
+
+    if(document.querySelector("#on-slider").classList.value==="game-turned-on"){
+        TURNED_ON = true;
+    }
+    
+    game = new Game();
+}
+
+//End Callable Functions
+
+
+//Page Ready
+document.addEventListener("DOMContentLoaded", function() { //start doing things once the DOM is ready to be manipulated
+    var game;
+
+    document.querySelector('#on-slider').addEventListener('click',function(){
+        turnOnOff(game); //initialize new Game object
     });
-  });
+
+});
